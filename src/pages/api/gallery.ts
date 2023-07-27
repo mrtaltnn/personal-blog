071eaf15ce4e562c9eed664006c526a7441971d2
mrtaltnn <mertaltun2@gmail.com>
2023-07-27 17:53:46 +0300

    feat(gallery component and gallery api): change photo hosting service to flickr

--- src/pages/api/gallery.ts
+++ src/pages/api/gallery.ts
@@ -4,9 +4,10 @@ export const get: APIRoute = async ({ params, request }) => {
   const url = new URL(request.url)
   const _page = (new URLSearchParams(url.search)).get("page") || "1";
   let page: number = parseInt(_page);
-  
 
-  const data = await getUnsplachUserPhotos(page);
+
+  // const data = await getUnsplachUserPhotos(page);
+  const data = await getFlickrUserPhotos(page);
 
   return new Response(JSON.stringify({
     data,
@@ -27,7 +28,7 @@ export const all: APIRoute = ({ request }) => {
   }
 }
 
-async function getUnsplachUserPhotos( page: number) {
+async function getUnsplachUserPhotos(page: number) {
   const endpoint = `${import.meta.env.UNSPLASH_API_URL}/users/${import.meta.env.UNSPLASH_USERNAME}/photos/?client_id=${import.meta.env.UNSPLASH_CLIENT_ID}&page=${page}`;
   const response = await fetch(endpoint);
   if (!response.ok) {
@@ -42,8 +43,42 @@ async function getUnsplachUserPhotos( page: number) {
       width: x.width,
       height: x.height,
       color: x.color,
-      blur_hash:x.blur_hash,
+      blur_hash: x.blur_hash,
       urls: x.urls,
+    };
+  });
+  return results;
+}
+
+async function getFlickrUserPhotos(page: number) {
+  const endpoint = `${import.meta.env.FLICKR_API_URL}/services/rest/?method=flickr.people.getPublicPhotos&api_key=${import.meta.env.FLICKR_API_KEY}&user_id=${import.meta.env.FLICKR_API_USER_ID}&extras=+url_o%2C+url_m&per_page=10&page=${page}&format=json&nojsoncallback=1`;
+
+  const response = await fetch(endpoint);
+  
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  const json = await response.json();
+  const photoList = json.photos.photo;
+  
+  var results = photoList.map(function (x: any) {
+    return {
+      id: x.id,
+      description: x.title,
+      alt: x.title,
+      urls: 
+        {
+          original: {
+            url: x.url_o,
+            w: x.width_o,
+            h: x.height_o
+          },
+          medium: {
+            url: x.url_m,
+            w: x.width_m,
+            h: x.height_m
+          }
+        }
     };
   });
   return results;
